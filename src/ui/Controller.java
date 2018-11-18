a8c5bf484d93dbdccb0e02c52fecc867779f4948
Naman Sethi <namansethi@cmail.carleton.ca>
2018-11-17 20:51:12 -0500

    General Structure

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,25 +1,68 @@
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
-import static java.lang.Thread.currentThread;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Controller {
 
     @FXML
-    private Pane red;
+    private Button loadButton;
     @FXML
-    private Pane blue;
-    @FXML
-    private Button button;
+    private ChoiceBox<String> dataChoiceBox;
 
+    private static final String PNG_SELECTOR_LABEL = "PNG FILE";
+    private static final String TEXT_SELECTOR_LABEL = "ASCII TEXT FILE";
 
 
     public void initialize(){
-        button.setOnMouseClicked(event->{
 
+        dataChoiceBox.setItems(FXCollections.observableArrayList(PNG_SELECTOR_LABEL, TEXT_SELECTOR_LABEL));
+        dataChoiceBox.getSelectionModel().selectFirst();
+
+
+
+        loadButton.setOnMouseClicked(actionEvent->{
+
+            FileChooser fileChooser = new FileChooser();
+            setFileExtension(dataChoiceBox, fileChooser);
+            File file = fileChooser.showOpenDialog(((Node) actionEvent.getTarget()).getScene().getWindow());
+
+            String dataSelectionValue = dataChoiceBox.getValue();
+            if (dataSelectionValue.equalsIgnoreCase(PNG_SELECTOR_LABEL)) {
+
+
+            } else if (dataSelectionValue.equalsIgnoreCase(TEXT_SELECTOR_LABEL)) {
+
+
+            }
         });
+    }
+
+    private void setFileExtension(ChoiceBox dataChoiceBox, FileChooser fileChooser) {
+
+        int selectedChoice = dataChoiceBox.getSelectionModel().getSelectedIndex() + 1;
+        FileChooser.ExtensionFilter extFilter;
+
+        switch (selectedChoice) {
+
+            case 1:
+                extFilter = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png");
+                fileChooser.getExtensionFilters().add(extFilter);
+                break;
+            case 2:
+                extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+                fileChooser.getExtensionFilters().add(extFilter);
+                break;
+        }
+
+
     }
 
 }
